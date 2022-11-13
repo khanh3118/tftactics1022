b07c86d42d0097bd71ec96f3f16a6c597bcd2191
Phan Đình Khánh <khanhphan036@gmail.com>
2022-11-13 22:42:43 +0700

    can't fix SSL  xD

--- src/services/metareport.js
+++ src/services/metareport.js
@@ -1,6 +1,6 @@
 async function getMetaComps() {
   let res = await fetch(
-    "http://13.212.204.133:8080/metareport"
+    "https://tftactic-crawl-meta.herokuapp.com/metareport"
   );
   let data = await res.json();
   return data[0].data;
